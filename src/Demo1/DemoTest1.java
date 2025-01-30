7f333989eb6c4469adf5e7dfe54f7906615c2cdb
elicaro <carolinatolosa02@gmail.com>
2025-01-30 10:55:41 -0500

    Update DemoTest1.java
    First Change

--- src/Demo1/DemoTest1.java
+++ src/Demo1/DemoTest1.java
@@ -15,9 +15,30 @@ public static void main (String args []) {
 			System.out.println("examTwo score is higher");
 		} else if ( examOne == examTwo) {
 			System.out.println("Both exam score is same");	
+		}	
+	
+}
+
+char setGender = 'M';
+		String getGender = "";
+
+		switch (setGender) {
+		case 'M':
+		case 'm':
+			getGender = "MALE";
+			break;
+		case 'F':
+		case 'f':
+			getGender = "FEMALE";;
+			break;
+		default:
+			getGender = "OTHERS";
 		}
-		
-		}
+		System.out.println(getGender);
+	}
+
+
+	
 	}
 
 
