ed556c64a2cc7032aa69933436cf32a17ae1bc67
carol <carol@Carolina>
2025-01-30 10:36:45 -0500

    Creating Method and Logic

--- src/Demo1/DemoTest1.java
+++ src/Demo1/DemoTest1.java
@@ -1,5 +1,24 @@
 package Demo1;
 
 public class DemoTest1 {
+	
+public static void main (String args []) {
+		
+		int examOne = 85;
+		int examTwo = 90;
+		
+		
+		
+		if (examOne > examTwo) {
+			System.out.println("examOne score is higher");
+		}else if (examOne < examTwo) {
+			System.out.println("examTwo score is higher");
+		} else if ( examOne == examTwo) {
+			System.out.println("Both exam score is same");	
+		}
+		
+		}
+	}
 
-}
+
+
